72a6661143e63e858de069241ef09e2cdd36430d
Abhishek Patel <itsabhishekpatel01@gmail.com>
2025-01-22 03:16:00 +0530

    Fixed import in App.jsx 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
 import { useAuthStore } from './store/useAuthStore'
 import { useThemeStore } from './store/useThemeStore'
-import SignupPage from './pages/SignUpPage'
+import SignupPage from './pages/SignupPage'
 
 const App = () => {
   const {authUser ,checkAuth, isCheckingAuth, onlineUsers} = useAuthStore();
